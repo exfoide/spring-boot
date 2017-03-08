299f2b2e8ef90d61eeae9727d2ff14a5c3385aa4
exfoide@126.com <exfoide@126.com>
2017-03-08 17:13:05 +0800

    9.spring boot使用jpa连接数据库

---
+++ spring-boot-jpa/src/main/java/com/jasper/Application.java
@@ -0,0 +1,14 @@
+package com.jasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Jasper on 2017/3/7.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
+}
